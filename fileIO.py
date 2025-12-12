409ac5d76d0e904626a991342eba747b24309381
sgarrow <stangarrow@yahoo.com>
2025-12-11 17:29:30 -0800

    spiClock. app v1.6.21, srv v1.7.1. Mod us cmd. Releasable.

--- fileIO.py
+++ fileIO.py
@@ -5,7 +5,7 @@ import datetime        as dt
 # Version number of the shared files.
 # Calling it the version of the "server".
 # As opposed to the version number of the "app" which is in cmdVectors.py
-VER = ' v1.7.0 - 10-Dec-2025'
+VER = ' v1.7.1 - 11-Dec-2025'
 
 def readFileWrk(parmLst, inFile):
     usage = ' Usage rlf [ numLines [start ["matchStr"]] ].'
