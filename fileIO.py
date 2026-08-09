8484215b75b34032371cf4b53bf346147f51a369
sgarrow <stangarrow@yahoo.com>
2026-08-08 20:02:53 -0700

    "v1.8.10. rlf/ref with a match string now returns line above the match as well as the line with the match.  Tine above has the time stamp."

--- fileIO.py
+++ fileIO.py
@@ -6,10 +6,28 @@ import datetime        as dt
 # Version number of the shared files.
 # Calling it the version of the "server".
 # As opposed to the version number of the "app" which is in cmdVectors.py
-VER = 'v1.8.9 - 01-Aug-2026'
+VER = 'v1.8.10 - 08-Aug-2026'
 
 def readFileWrk(parmLst, inFile):
-    usage = ' Usage rlf [ numLines [start ["matchStr"]] ].'
+
+    usage = \
+ '''
+Example parameters:
+
+ <NONE>    - return 5 lines starting from - 5th line from bottom.
+ 3         - return 3 lines starting from - 3rd line from bottom.
+ 3 4       - return 3 lines starting from - line 4 (0 is 1st line).
+ 
+ "match"   - return all lines with    "match" in it.
+ 3 "match" - return last 3 lines with "match" in it.
+ "match", 3 - return last 3 lines with "match" in it.
+
+ Note: '"match this"' (two words) not supported (too many parms).
+
+'''
+
+    print(parmLst) # All parameters get passed in as strings.
+    numParms = len(parmLst)
 
     # Get total Lines in file.
     try:
@@ -17,58 +35,99 @@ def readFileWrk(parmLst, inFile):
             numLinesInFile = sum(1 for line in f)
     except FileNotFoundError:
         return ' Could not open file {} for reading'.format(inFile)
+    ###########################
 
-    # Get/Calc number of lines to return (parmLst[0]).
-    try:
-        numLinesToRtnA = int(parmLst[0])
-    except ValueError:
-        return ' Invalid number of lines to read.\n' + usage
+    # Verify correct number of parameters
+    if not 0 < numParms < 3:
+        return ' Incorrect number of parameters.' + usage
+    ###########################
 
-    numLinesToRtn = min( numLinesToRtnA, numLinesInFile )
-    numLinesToRtn = max( numLinesToRtn,  1 ) # Don't allow reading 0 lines.
+    # See if there is (at most) 1 parameter of the form '"match"'
+    # Note: '"match this"' (two words) not supported.
+    matchStr    = ''
+    numMatchStr = 0
+    for idx,el in enumerate(parmLst):
+        if el == '""':
+            return ' Empty match string not supported.' + usage
+        if el.startswith('"') and el.endswith('"'):
+            matchSrtAtIdx = idx
+            matchStr      = el[1:-1]
+            numMatchStr  += 1
+        if numMatchStr > 1:
+            return ' Multiple match strings not supported.' + usage
+    ###########################
 
-    # Get/Calc startIdx (parmLst[1]).
-    if len(parmLst) > 1:
+    # If match str provided, get number of matches to return.
+    # The start/end file index is hardcoded to 0, and nm lines in file.
+    numLinesToRtn   = 0 # Loop control when no match string supplied
+    numMatchesToRtn = 0 # Loop control when  q match string supplied 
+    if numMatchStr:
+        if numParms > 1:
+            idxToCastToInt = 0 if matchSrtAtIdx == 1 else 1
+            try:
+                numMatchesToRtn = int(parmLst[idxToCastToInt])
+            except ValueError:
+                return ' Invalid number of matches to return.\n' + usage
+        else:
+            numMatchesToRtn = 'all'
+
+        startIdx = 0
+        endIdx   = numLinesInFile-1
+    ###########################
+    else: # No match str specified.
+
+        # Get/Calc number of lines to return (parmLst[0]).
         try:
-            startIdx = max(int(parmLst[1]),0)
+            numLinesToRtnA = int(parmLst[0])
         except ValueError:
-            return ' Invalid startIdx.\n' + usage
+            return ' Invalid number of lines to read.\n' + usage
 
-        if startIdx > numLinesInFile:
+        numLinesToRtn = min(numLinesToRtnA, numLinesInFile)
+        numLinesToRtn = max(numLinesToRtn,  1) # Don't allow reading <= 0 lines.
+
+        # Get/Calc startIdx (parmLst[1]).
+        if len(parmLst) > 1:
+            try:
+                startIdx = max(int(parmLst[1]),0) # Don't allow starting
+            except ValueError:                    # before 0th line.
+                return ' Invalid startIdx.\n' + usage
+
+            if startIdx > numLinesInFile:
+                startIdx=max( numLinesInFile - numLinesToRtn, 0 ) # Can't start
+        else:                                                     # after EOF.
             startIdx = max(numLinesInFile - numLinesToRtn, 0)
+
+        # Calc endIdx.
+        endIdx = max(startIdx + numLinesToRtn - 1, 0)
+        endIdx = min(endIdx, numLinesInFile-1)
+    ###########################
+
+    rspStr  = ' numLinesInFile  = {:4}.\n'.format( numLinesInFile  )
+    rspStr += ' numMatchesToRtn = {:4}.\n'.format( numMatchesToRtn )
+    rspStr += '  numLinesToRtn  = {:4}.\n'.format( numLinesToRtn   )
+    rspStr += '       startIdx  = {:4}.\n'.format( startIdx        )
+    rspStr += '         endIdx  = {:4}.\n'.format( endIdx          )
+    rspStr += '       matchStr  = {}.\n\n'.format( matchStr        )
+
+    if numMatchStr > 0:
+        prevLine = '\n'
+        prevIdx  = -1
+        with open( inFile, 'r',encoding='utf-8') as f:
+            for currIdx,currLine in enumerate(f):
+                if startIdx <= currIdx <= endIdx:
+                    if matchStr in currLine:
+                        rspStr += ' {:4} - {}'.format(prevIdx,prevLine)
+                        rspStr += ' {:4} - {}\n'.format(currIdx,currLine)
+                    prevIdx  = currIdx
+                    prevLine = currLine
+
     else:
-        startIdx = max(numLinesInFile - numLinesToRtn, 0)
-
-    # Calc endIdx.
-    endIdx = max(startIdx + numLinesToRtn - 1, 0)
-    endIdx = min(endIdx, numLinesInFile-1)
-
-    # Build matchStr.
-    numDoubleQuote = 0
-    for el in parmLst[2:]:
-        if '"' in el:
-            numDoubleQuote += el.count('"')
-    matchStrEntered = len(parmLst) > 2
-
-    matchStr = ''
-    if matchStrEntered and numDoubleQuote == 2:
-        startStr = ' '.join(x for x in parmLst[2:] )
-        idxLst = [match.start() for match in re.finditer('"', startStr)]
-        matchStr = startStr[idxLst[0]+1:idxLst[1]]
-
-    rspStr  = ' numLinesInFile = {:4}.\n'.format( numLinesInFile )
-    rspStr += '  numLinesToRtn = {:4}.\n'.format( numLinesToRtn  )
-    rspStr += '       startIdx = {:4}.\n'.format( startIdx       )
-    rspStr += '         endIdx = {:4}.\n'.format( endIdx         )
-    rspStr += '       matchStr = {}.\n\n'.format( matchStr       )
-
-    with open( inFile, 'r',encoding='utf-8') as f:
-        for idx,line in enumerate(f):
-            if startIdx <= idx <= endIdx:
-                if matchStr != '' and matchStr in line:
-                    rspStr += ' {:4} - {}'.format(idx,line)
-                elif matchStr == '':
-                    rspStr += ' {:4} - {}'.format(idx,line)
+        with open( inFile, 'r',encoding='utf-8') as f:
+            for currIdx,currLine in enumerate(f):
+                if startIdx <= currIdx <= endIdx:
+                    rspStr += ' {:4} - {}'.format(currIdx,currLine)
+                    prevIdx  = currIdx
+                    prevLine = currLine
 
     return rspStr
 #############################################################################
@@ -102,3 +161,41 @@ def clearFile(parmLst):
 #        f.write( inStr )
 #        f.flush()
 ##############################################################################
+
+if __name__ == '__main__':
+
+    #            - return 5 lines starting from - 5th line from bottom.
+    # 3          - return 3 lines starting from - 3rd line from bottom.
+    # 3, 4       - return 3 lines starting from - line 4 (0 is 1st line).
+    #
+    # "match"    - return all lines with    "match" in it.
+    # 3, "match" - return last 3 lines with "match" in it.
+
+    while True:
+
+        params = ['logFile.txt',['5']]
+
+        inputStr = input( '--> ')
+        inputWords = inputStr.split()
+
+        if inputWords == []:       # In case user entered just spaces.
+            print( 'no command entered' )
+            continue
+
+        choice     = inputWords[0]
+        optArgsStr = inputWords[1:]
+
+        if choice == 'q':
+            break
+
+        if choice != 'rlf':
+            print( 'rlf not entered entered' )
+            continue
+
+        if len(optArgsStr) > 0:
+            params[1] = optArgsStr
+
+        #print(params)
+        rsp = readFile( params )
+        print()
+        print(rsp[0])
